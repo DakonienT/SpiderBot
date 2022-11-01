5f21ec60cb46b3a9108081053012554ae8680776
DakonienT <38476962+DakonienT@users.noreply.github.com>
2022-11-01 15:01:32 +0100

    Added debug on camera streamer

--- CameraStreamer/Client.py
+++ CameraStreamer/Client.py
@@ -1,9 +1,15 @@
 import cv2, socket, pickle, os  
+import logging
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 10000000)
 serverip="127.0.0.1"
 serverport=6666
 cap = cv2.VideoCapture(0)
+logging.basicConfig(format='%(asctime)s %(message)s')
+logging.root.setLevel(logging.NOTSET)
+logging.basicConfig(level=logging.NOTSET)
+count = 0 #Frames counter
+count_sent = 0
 while True:    
     ret,photo = cap.read()    
     
@@ -11,9 +17,13 @@ while True:
     
     ret, buffer = cv2.imencode(".jpg", photo,[int(cv2.IMWRITE_JPEG_QUALITY),70])    
     x_as_bytes = pickle.dumps(buffer)    
-    
-    s.sendto(x_as_bytes,(serverip , serverport))    
-    
+    count +=1
+    try:
+        s.sendto(x_as_bytes,(serverip , serverport))
+        count_sent += 1 #Increment counter
+    except:
+        pass
+    logging.debug(str(count_sent) + "/" + str(count) + " Images sent to " + serverip + ":" + str(serverport))
     if cv2.waitKey(10) == 13:        
         
           break  
